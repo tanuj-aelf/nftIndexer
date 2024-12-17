6debdb27a156ac2364b229d44323b14fed06eb1d
Tanuj Kushwah <tanuj.singh@aelf.io>
2024-12-17 23:59:43 +0530

    added tokenInfo fetch code using Aelf Client

--- src/nftIndexer/obj/Debug/net8.0/nftIndexer.AssemblyInfo.cs
+++ src/nftIndexer/obj/Debug/net8.0/nftIndexer.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("nftIndexer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4490ba63276dcf76f52529fb948cf83fb519e1f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("nftIndexer")]
